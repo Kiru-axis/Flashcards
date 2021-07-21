68be70cec67177b2e97e6defad9cba9c3bdd52e4
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-21 10:59:05 +0300

    debugging api url

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -7,7 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path("notes/", views.notes, name='notes'),
-    path("api/notes_list/",views.NotesList.as_view(),name='notes_api')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
